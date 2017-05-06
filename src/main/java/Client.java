89fc0d4b4310bb4c21b4fe4c09c14d023684c41e
Yonkokilasi <yonkokilasi@gmail.com>
2017-05-06 21:24:09 +0300

    Completed adding of attributes to clients and connecting to database

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -35,9 +35,14 @@ public class Client {
     }
     public void save() {
         try(Connection con = DB.sql2o.open()) {
-            String sql = "SELECT *FROM clients where id=:id";
-            Client client = con.createQuery(sql).addParameter("id",id).executeAndFetchFirst(Client.class);
-            return client;
+            String sql = "INSERT INTO clients(name, stylistId) VALUES (:name, :stylistId)";
+            this.id = (int) con.createQuery(sql, true).addParameter("name", this.name).addParameter("stylistId", this.stylistId).executeUpdate().getKey();
+        }
+    }
+    public static Client find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "UPDATE client SET name = :name WHERE id = :id";
+            con.createQuery(sql).addParameter("name",name).addParameter("id",id).executeUpdate();
         }
     }
 

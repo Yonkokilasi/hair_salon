7251be132f3a3680b9f54fff4b906c4004a18918
Yonkokilasi <yonkokilasi@gmail.com>
2017-05-06 22:25:33 +0300

    begun to confugire route for clients

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,19 +11,29 @@ public class App {
     return Integer.parseInt(processBuilder.environment().get("PORT"));
   }
   return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-}
-  public static void main(String[] args) {
+    }
+    public static void main(String[] args) {
       staticFileLocation("/public");
       String layout = "templates/layout.vtl";
 
-    port(getHerokuAssignedPort());
-    staticFileLocation("/public");
+      port(getHerokuAssignedPort());
+      staticFileLocation("/public");
 
-    get("/", (request, response)-> {
+        get("/", (request, response)-> {
         Map<String, Object>model = new HashMap<String, Object>();
         model.put("template","templates/index.vtl");
         return new ModelAndView(model,layout);
-    }, new VelocityTemplateEngine());
+        }, new VelocityTemplateEngine());
+
+        get("/clients", (request, response)-> {
+        Map<String, Object> model = new HashMap<String , Object>();
+        model.put("clients",Client.all());
+        model.put("template","templates/clients.vtl");
+        return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        post("/clients")
+
+    }
 
 }
-    }

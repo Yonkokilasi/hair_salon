e61f7385a13e4e1a65f91dd811d2b8111695c9fc
Yonkokilasi <yonkokilasi@gmail.com>
2017-05-08 13:03:47 +0300

    Renamed client and stylist. java files

---
+++ src/main/java/Clients.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
+
+public class Client {
+    private String name;
+    private int id;
+    private int stylistId;
+
+
+    public Client(String name, int stylistId) {
+    this.name = name;
+    this.stylistId = stylistId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getStylistId() {
+        return stylistId;
+    }
+    public static List<Client> all() {
+        String sql = "SELECT  id , name, stylistId FROM clients";
+            try(Connection con = DB.sql2o.open()) {
+                return con.createQuery(sql).executeAndFetch(Client.class);
+            }
+    }
+    @Override
+    public boolean equals(Object otherClient) {
+        if (!(otherClient instanceof Client)) {
+            return false;
+        } else {
+            Client newClient = (Client) otherClient;
+            return this.getName().equals(newClient.getName());
+        }
+    }
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO clients(name, stylistId) VALUES (:name, :stylistId)";
+            this.id = (int) con.createQuery(sql, true).addParameter("name", this.name).addParameter("stylistId", this.stylistId).executeUpdate().getKey();
+        }
+    }
+        public static Client find(int id) {
+            try(Connection con = DB.sql2o.open()) {
+                String sql = "SELECT * FROM clients WHERE id=:id";
+                Client client = con.createQuery(sql).addParameter("id",id).executeAndFetchFirst(Client.class);
+                return client;
+            }
+        }
+        public void delete() {
+            try(Connection con = DB.sql2o.open()) {
+                String sql = "DELTE FROM clients WHERE id = :id;";
+                con.createQuery(sql).addParameter("id",id).executeUpdate();
+            }
+        }
+
+
+}
---
+++ src/main/java/Stylists.java
@@ -0,0 +1,64 @@
+import java.util.List;
+import java.util.ArrayList;
+import org.sql2o.*;
+
+public class Stylist {
+    private String name;
+    private int id;
+    private String specialty;
+
+    public Stylist(String name, String specialty) {
+        this.name = name;
+        this.specialty = specialty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public static List<Stylist> all() {
+        String sql = "SELECT id , name , specialty FROM stylists";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Stylist.class);
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Stylist find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+          String sql = "SELECT * FROM stylists WHERE id=:id";
+          Stylist stylist = con.createQuery(sql)
+            .addParameter("id", id)
+            .executeAndFetchFirst(Stylist.class);
+          return stylist;
+        }
+    }
+    @Override
+    public boolean equals(Object otherStylist) {
+      if (!(otherStylist instanceof Stylist)) {
+        return false;
+      } else {
+        Stylist newStylist = (Stylist) otherStylist;
+        return this.getName().equals(newStylist.getName()) &&
+                this.getSpecialty().equals(newStylist.getSpecialty());
+      }
+    }
+
+     public void save() {
+       try(Connection con = DB.sql2o.open()) {
+         String sql = "INSERT INTO stylists(name,specialty) VALUES (:name,:specialty)";
+         this.id = (int) con.createQuery(sql, true)
+           .addParameter("name", this.name)
+           .addParameter("specialty", this.specialty)
+           .executeUpdate()
+           .getKey();
+       }
+     }
+}
